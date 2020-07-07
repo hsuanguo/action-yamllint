ac36adf35939ee2b95d2d73d179f5ea2b3dad997
hsuanguo <hsuan.guo@gmail.com>
2020-07-07 13:18:52 +0100

    path fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 yamllint --version
 
-yamllint -c "config.yaml" --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} \
+yamllint -c "/config.yaml" --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} \
   | reviewdog -efm="%f:%l:%c: %m" -name "yamllint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
