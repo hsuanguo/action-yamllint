d71950faad24ecc75a2e6c66607bbac018625abb
hsuanguo <hsuan.guo@gmail.com>
2020-07-13 18:30:49 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo "I am inside the docker container "
+
 yamllint --version
 
 yamllint -c "/config.yaml" --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} \
